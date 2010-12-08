5e6cbbdb28987fe758bbde5884f223b4685a2ad9
Kazutaka Miyasaka <kazmiya@gmail.com>
2010-12-08 17:19:44 +0900

    fit action.php to my current coding standard

--- action.php
+++ action.php
@@ -1,36 +1,47 @@
 <?php
 /**
- * DokuWiki Plugin AutoIndentControl
+ * AutoIndentControl Plugin for DokuWiki / action.php
  *
  * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author  Kazutaka Miyasaka <kazmiya@gmail.com>
  */
 
 // must be run within DokuWiki
-if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_INC')) {
+    die();
+}
 
-require_once(DOKU_PLUGIN.'action.php');
+if (!defined('DOKU_PLUGIN')) {
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+}
 
-class action_plugin_autoindentcontrol extends DokuWiki_Action_Plugin {
+require_once DOKU_PLUGIN . 'action.php';
+
+class action_plugin_autoindentcontrol extends DokuWiki_Action_Plugin
+{
     /**
      * Returns some info
      */
-    function getInfo() {
-        return confToHash(DOKU_PLUGIN.'autoindentcontrol/plugin.info.txt');
+    function getInfo()
+    {
+        return confToHash(DOKU_PLUGIN . 'autoindentcontrol/plugin.info.txt');
     }
 
     /**
      * Registers an event handler
      */
-    function register(&$controller) {
-        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, '_exportToJSINFO');
+    function register(&$controller)
+    {
+        $controller->register_hook(
+            'DOKUWIKI_STARTED', 'AFTER', $this, 'exportToJSINFO'
+        );
     }
 
     /**
      * Exports configuration settings to $JSINFO
      */
-    function _exportToJSINFO(&$event) {
+    function exportToJSINFO(&$event)
+    {
         global $JSINFO;
 
         $JSINFO['plugin_autoindentcontrol'] = array(
